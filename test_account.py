c4e1f100ac0ebcde6d974a010cfceee7777aeffd
Evan Carlile <114954921+ecar33@users.noreply.github.com>
2023-04-09 17:23:50 -0500

    Created account and test_account file

--- test_account.py
+++ test_account.py
@@ -1,13 +1,42 @@
+import pytest
+
 from account import *
 
-def main():
-    test_account = Account("Steve Jobs")
-    test_account.deposit(6.3333333)
-    print(test_account.get_name())
-    print(test_account.get_balance())
+def test_init():
+    test_account_1 = Account("Steve")
+    assert test_account_1.account_balance == 0
+    assert test_account_1.account_name == "Steve"
+    test_account_2 = Account(0)
+    assert test_account_2.account_balance == 0
+    assert test_account_2.account_name == 0
+def test_deposit():
+    account = Account("test")
+    assert account.deposit(0) == True
+    assert account.deposit("test") == False
+    assert account.deposit(3.1414141414) == True
+    assert account.get_balance() == 3.1414141414
+    assert account.deposit(-1) == False
+    assert account.deposit(9999999999) == True
+    assert account.get_balance() == pytest.approx(10000000002.14, .01)
+    assert account.deposit('') == False
+
+def test_withdraw():
+    account = Account("test")
+    assert account.deposit(30) == True
+    assert account.get_balance() == 30
+    assert account.withdraw("test") == False
+    assert account.get_balance() == 30
+    assert account.withdraw(10) == True
+    assert account.get_balance() == 20
+    assert account.deposit(-1) == False
+    assert account.get_balance() == 20
+    assert account.withdraw('') == False
+    assert account.get_balance() == 20
+    assert account.withdraw(20) == True
+    assert account.get_balance() == 0
 
 
 
 
-if __name__ == "__main__":
-    main()
+
+
---
+++ testinaccount.py
@@ -0,0 +1,8 @@
+from account import *
+
+
+print(True)
+
+# account = Account("Steve Jobs")
+# print(account.deposit(False))
+# print(account.get_balance())
