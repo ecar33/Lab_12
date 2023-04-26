fa0cafdea42f569731effe33765b76761ba34488
Evan Carlile <114954921+ecar33@users.noreply.github.com>
2023-04-26 15:49:27 -0500

    Updated to make attributes private

--- account.py
+++ account.py
@@ -5,18 +5,19 @@ class Account:
 
     def __init__(self, name: str):
         """
-
-        :param self.account_name: Person's name
-        :param self.account_balance: Person's account balance
+        Function to set up account object.
+        :param self.__account_name: Person's name
+        :param self.__account_balance: Person's account balance
 
         """
-        self.account_name = name
-        self.account_balance = 0
+        self.__account_name = name
+        self.__account_balance = 0
 
     def deposit(self, amount: float) -> bool:
         """
         Method to deposit funds from account
         :param amount: Amount to deposit
+        :return: Success/failure to deposit
 
         """
         try:
@@ -24,7 +25,7 @@ class Account:
             amount = float(amount)
             if amount >= 0:
                 # Check if amount is positive
-                self.account_balance += amount
+                self.__account_balance += amount
                 return True
             else:
                 return False
@@ -35,14 +36,15 @@ class Account:
         """
         Method to withdraw funds from account
         :param amount: Amount to withdraw
+        :return: Success/failure to withdraw
 
         """
         try:
             # Check if amount is a float
             amount = float(amount)
-            if (amount >= 0) and (amount <= self.account_balance):
+            if (amount >= 0) and (amount <= self.get_balance()):
                 # Check if amount is positive and less than or equal to current balance
-                self.account_balance -= float(amount)
+                self.__account_balance -= float(amount)
                 return True
             else:
                 return False
@@ -51,16 +53,16 @@ class Account:
 
     def get_balance(self) -> float:
         """
-
+        Method to obtain the account's balance
         :return: Account balance
 
         """
-        return self.account_balance
+        return self.__account_balance
 
     def get_name(self) -> str:
         """
-
+        Method to obtain the account's name
         :return: Account name
 
         """
-        return self.account_name
+        return self.__account_name
